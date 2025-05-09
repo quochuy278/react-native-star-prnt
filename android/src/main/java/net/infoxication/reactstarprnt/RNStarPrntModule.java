6878366aaabca2d166e9de22c63d20b568f51980
Huy Bui <55485803+quochuy278@users.noreply.github.com>
2025-05-09 18:36:52 +0300

    Update RNStarPrntModule.java

--- android/src/main/java/net/infoxication/reactstarprnt/RNStarPrntModule.java
+++ android/src/main/java/net/infoxication/reactstarprnt/RNStarPrntModule.java
@@ -164,7 +164,7 @@ public class RNStarPrntModule extends ReactContextBaseJavaModule {
 
     String portSettings = getPortSettingsOption(emulation);
     if (starIoExtManager != null && starIoExtManager.getPort() != null) {
-      starIoExtManager.disconnect(null);
+     starIoExtManager.disconnect((IConnectionCallback)null);
     }
     starIoExtManager = new StarIoExtManager(hasBarcodeReader ? StarIoExtManager.Type.WithBarcodeReader : StarIoExtManager.Type.Standard, portName, portSettings, 10000, context);
     starIoExtManager.setListener(starIoExtManagerListener);
